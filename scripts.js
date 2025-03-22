e7174e3928ef3b33fd2d92a18ae52b40f291eadd
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-03-22 12:19:18 -0600

    feat(board): add function to log board

--- scripts.js
+++ scripts.js
@@ -2,3 +2,17 @@ const ROW_WIDTH = 5;
 const COL_HEIGHT = 4;
 
 const board = Array(ROW_WIDTH * COL_HEIGHT).fill(null);
+
+function logBoard() {
+    for (let rowNum = 0; rowNum < COL_HEIGHT; rowNum++) {
+        const rowItems = [];
+        const startIndex = rowNum * ROW_WIDTH;
+        const endIndex = startIndex + ROW_WIDTH;
+        
+        for (let rowItem = startIndex; rowItem < endIndex; rowItem++) {
+            rowItems.push(board[rowItem]);
+        }
+        
+        console.log(rowItems);
+    }
+}
