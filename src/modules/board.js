1c2c61e3aa8d245b0558e0ed589660c48eb9dde5
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-04-19 18:25:18 -0600

    feat(board): add function to check rows for winner
    Check whether any row has the specified token WIN_LENGTH times in a row.

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,5 +1,6 @@
 const ROW_WIDTH = 5;
 const COL_HEIGHT = 4;
+const WIN_LENGTH = 4;
 
 // Format board as [row][col]
 const board = Array(COL_HEIGHT).fill(null);
@@ -29,6 +30,22 @@ function dropToken(token, col) {
     const lowestAvailableCell = availableCells[availableCells.length - 1];
     board[lowestAvailableCell][col] = token;
     return true;
+}
+
+function rowHasWinner(token) {
+    const rowHasWinner = board.some(row => {
+        for (let n = 0; n < ROW_WIDTH - WIN_LENGTH + 1; n++) {
+            const subsetStart = n;
+            const subsetEnd = n + ROW_WIDTH - 1;
+            const subset = row.slice(subsetStart, subsetEnd);
+            
+            if (subset.every(item => item === token)) {
+                return true;
+            }
+        }
+    })
+
+    return rowHasWinner;
 }
 
 export { logBoard, clearBoard, dropToken };
